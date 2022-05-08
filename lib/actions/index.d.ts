911004f28fe0c08a619a8b9a704f777159381431
maram-alshen-repzo <maram.alshen@repzoapp.com>
2022-05-08 09:26:06 +0300

    delete /lib

--- lib/actions/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Config, Action } from "../types";
-import { EVENT } from "../types";
-export declare const actions: (event: EVENT, options: Config) => Promise<void>;
-export declare const actionsList: Action[];
--- lib/commands/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Command, CommandEvent } from "./../types";
-export declare const commands: (CommandEvent: CommandEvent) => Promise<any>;
-export declare const commandsList: Command[];
